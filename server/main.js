b30e7f481dd868b18cdd2ef84f817254a746ca4b
Saysan777 <alokaryal32@gmail.com>
2022-05-12 00:43:59 +0545

    mongo

--- server/main.js
+++ server/main.js
@@ -1,2 +1,8 @@
-import "../imports/api/fixtures";
-import { links } from "../imports/api/collections/Links.js";
+// import "../imports/api/fixtures";
+// import { links } from "../imports/api/collections/Links.js";
+import { Meteor } from "meteor/meteor";
+
+Meteor.startup(() => {
+  process.env["MONGO_URL"] =
+    "mongodb+srv://alok:alok@cluster0.jjo2x.mongodb.net/meteor?retryWrites=true&w=majority";
+});
